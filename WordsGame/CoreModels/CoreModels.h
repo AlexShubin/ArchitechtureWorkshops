e8c23389e832250b9a94dc8e588dcd10d4f7f38c
Alex Shubin <alexshubin22@gmail.com>
2020-09-04 12:15:36 +0200

    work shop 1

---
+++ WordsGame/CoreModels/CoreModels.h
@@ -0,0 +1,19 @@
+//
+//  CoreModels.h
+//  CoreModels
+//
+//  Created by ashubin on 04.09.20.
+//  Copyright © 2020 Alex Shubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreModels.
+FOUNDATION_EXPORT double CoreModelsVersionNumber;
+
+//! Project version string for CoreModels.
+FOUNDATION_EXPORT const unsigned char CoreModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreModels/PublicHeader.h>
+
+
---
+++ WordsGame/WordsGame/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  WordsGame
+//
+//  Created by ashubin on 04.09.20.
+//  Copyright © 2020 Alex Shubin. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
