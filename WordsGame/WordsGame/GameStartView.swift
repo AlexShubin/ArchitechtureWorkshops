9e8f053b6d3e8d3928388e8e9b75a4674f3702eb
Alex Shubin <alexshubin22@gmail.com>
2020-09-09 16:41:23 +0200

    workshop 2 (#6)

---
+++ WordsGame/WordsGame/GameStartView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct GameStartView: View {
+    @ObservedObject var store: AppStore
+
+    var body: some View {
+        Button(action: { self.store.startGame() },
+               label: { Text("Start game") })
+            .font(.largeTitle)
+            .sheet(isPresented: Binding.constant(store.state.isGameStarted),
+                   onDismiss: { self.store.stopGame() },
+                   content: { GameView(store: self.store) })
+    }
+}
+
+struct GameStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameStartView(store: AppStore(initialState: AppState()))
+    }
+}
