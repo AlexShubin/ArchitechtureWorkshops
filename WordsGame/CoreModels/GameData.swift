e7fc14ab66a412432ec25bc87060eb2c658bca22
Alex Shubin <alexshubin22@gmail.com>
2020-10-05 11:36:54 +0200

    remove hardcoded stuff

--- WordsGame/CoreModels/GameData.swift
+++ WordsGame/CoreModels/GameData.swift
@@ -23,15 +23,3 @@ public struct RoundData: Equatable {
         self.isTranslationCorrect = isTranslationCorrect
     }
 }
-
-public extension GameData {
-    static let `default` = GameData(rounds: [
-        .init(questionWord: "Yes", answerWord: "Нет", isTranslationCorrect: false),
-        .init(questionWord: "Cat", answerWord: "Кошка", isTranslationCorrect: true),
-        .init(questionWord: "Good", answerWord: "Хорошо", isTranslationCorrect: true),
-        .init(questionWord: "Bad", answerWord: "Плохо", isTranslationCorrect: true),
-        .init(questionWord: "No", answerWord: "Да", isTranslationCorrect: false),
-        .init(questionWord: "Dog", answerWord: "Собака", isTranslationCorrect: true),
-        .init(questionWord: "Hi", answerWord: "Привет", isTranslationCorrect: true)
-    ])
-}
